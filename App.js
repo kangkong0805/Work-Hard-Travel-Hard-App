f4ae76ed8740a0e7bd101a2b5b5fe4c677d6b07e
kangkong <kang3530554@gmail.com>
2023-07-06 14:17:26 +0900

    ✨ Add: to do 데이터를 관리하는 state 추가

--- App.js
+++ App.js
@@ -13,9 +13,25 @@ import { useState } from "react";
 export default function App() {
   const [working, setWorking] = useState(true);
   const [text, setText] = useState("");
+  const [toDos, setToDos] = useState({});
+
   const travel = () => setWorking(false);
   const work = () => setWorking(true);
   const onChangeText = (payload) => setText(payload);
+  const addToDo = () => {
+    if (text === "") return;
+
+    // Object.assign(target, ...sources)
+    // Object.assign()은 sources에서 target으로 복사하고 target을 반환
+    const newToDos = Object.assign({}, toDos, {
+      [Date.now()]: { text, work: working },
+    });
+
+    // save to do
+    setToDos(newToDos);
+    setText("");
+  };
+  console.log(toDos);
 
   return (
     <View style={styles.container}>
@@ -38,9 +54,11 @@ export default function App() {
       </View>
       <View>
         <TextInput
+          onSubmitEditing={addToDo}
+          onChangeText={onChangeText} // 텍스트 변환 이벤트 발생 시 실행
+          returnKeyType="done"
           placeholder={working ? "Add a To Do" : "Where do you want to go?"}
           style={styles.input}
-          onChangeText={onChangeText} // 텍스트 변환 이벤트 발생 시 실행
           value={text}
         ></TextInput>
       </View>
