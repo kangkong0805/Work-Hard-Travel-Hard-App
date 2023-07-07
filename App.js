1fe49a337ea2c6e2f27e080ca44a536989273074
kangkong <kang3530554@gmail.com>
2023-07-07 14:31:34 +0900

    ✨ Add: To Do 수정 기능 추가

--- App.js
+++ App.js
@@ -1,3 +1,5 @@
+import { AntDesign, Fontisto, Ionicons } from "@expo/vector-icons";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import { StatusBar } from "expo-status-bar";
 import { useEffect, useState } from "react";
 import {
@@ -10,8 +12,6 @@ import {
 } from "react-native";
 import { theme } from "./colors";
 import styles from "./style";
-import AsyncStorage from "@react-native-async-storage/async-storage";
-import { Fontisto, AntDesign } from "@expo/vector-icons";
 
 const STORAGE_KEY = "@toDos";
 const WORKING = "@working";
@@ -20,6 +20,7 @@ export default function App() {
   const [working, setWorking] = useState(true);
   const [text, setText] = useState("");
   const [toDos, setToDos] = useState({});
+  const [modifyText, setModifyText] = useState("");
 
   const travel = () => setWorking(false);
   const work = () => setWorking(true);
@@ -43,7 +44,7 @@ export default function App() {
     if (text === "") return;
     const newToDos = {
       ...toDos,
-      [Date.now()]: { text, working: working, check: false },
+      [Date.now()]: { text, working: working, check: false, modify: false },
     };
     setToDos(newToDos);
     await saveToDos(newToDos);
@@ -66,17 +67,22 @@ export default function App() {
     ]);
   };
 
-  const changeCheck = async (key) => {
+  const changeState = async (id, state) => {
+    const key = Object.keys(state)[0];
+    const value = Object.values(state)[0];
     const newToDos = {
       ...toDos,
-      [key]: {
-        text: toDos[key].text,
-        working: toDos[key].working,
-        check: !toDos[key].check,
+      [id]: {
+        ...toDos[id],
+        [key]: value,
       },
     };
     setToDos(newToDos);
     await saveToDos(newToDos);
+    if (key === "modify") {
+      console.log(toDos);
+      setModifyText(toDos[id].text);
+    }
   };
 
   const saveTheme = async () => {
@@ -121,8 +127,8 @@ export default function App() {
       </View>
       <View>
         <TextInput
-          onSubmitEditing={addToDo}
-          onChangeText={onChangeText} // 텍스트 변환 이벤트 발생 시 실행
+          onSubmitEditing={addToDo} // 입력 완료하면 실행
+          onChangeText={onChangeText} // 텍스트가 변화할 때 실행
           returnKeyType="done"
           placeholder={working ? "Add a To Do" : "Where do you want to go?"}
           style={styles.input}
@@ -132,23 +138,45 @@ export default function App() {
           {Object.keys(toDos).map((key) =>
             toDos[key].working === working ? (
               <View key={key} style={styles.toDo}>
-                <Text
-                  style={{
-                    ...styles.toDoText,
-                    textDecorationLine: toDos[key].check
-                      ? "line-through"
-                      : "none",
-                    color: toDos[key].check ? "#aaa" : "#fff",
-                  }}
-                >
-                  {toDos[key].text}
-                </Text>
+                {toDos[key].modify ? (
+                  <TextInput
+                    value={modifyText}
+                    onSubmitEditing={() =>
+                      changeState(key, { text: modifyText })
+                    }
+                    onChangeText={(payload) => setModifyText(payload)}
+                    returnKeyType="done"
+                    style={styles.toDoText}
+                  />
+                ) : (
+                  <Text
+                    style={{
+                      ...styles.toDoText,
+                      textDecorationLine: toDos[key].check
+                        ? "line-through"
+                        : "none",
+                      color: toDos[key].check ? "#aaa" : "#fff",
+                    }}
+                  >
+                    {toDos[key].text}
+                  </Text>
+                )}
                 <View style={styles.iconBox}>
                   <AntDesign
                     name={toDos[key].check ? "checksquareo" : "checksquare"}
                     size={18}
                     color="white"
-                    onPress={() => changeCheck(key)}
+                    onPress={() =>
+                      changeState(key, { check: !toDos[key].check })
+                    }
+                  />
+                  <Ionicons
+                    name="pencil"
+                    size={18}
+                    color="white"
+                    onPress={() =>
+                      changeState(key, { modify: !toDos[key].modify })
+                    }
                   />
                   <TouchableOpacity onPress={() => deleteToDo(key)}>
                     <Fontisto name="trash" size={18} color="red" />
--- style.js
+++ style.js
@@ -42,7 +42,7 @@ const styles = StyleSheet.create({
 
   iconBox: {
     flexDirection: "row",
-    gap: 10,
+    gap: 15,
   },
 });
 
